ed7a207f65ff3ee7e2842fac06f434703ebbc9a1
melMass <melmassadian@gmail.com>
2022-09-07 15:07:25 +0000

    Deploying to gh-pages from @ teia-community/teia-logos@783c918d5117f74b23f02f8a3efad0007b79c257 🚀

---
+++ _app/immutable/chunks/1-e7f662f7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-c9773675.js";export{t as component};
---
+++ _app/immutable/chunks/store-104f7d2d.js
@@ -0,0 +1,1 @@
+import{R as t}from"./singletons-2241f4ca.js";let r=t("light");export{r as t};
